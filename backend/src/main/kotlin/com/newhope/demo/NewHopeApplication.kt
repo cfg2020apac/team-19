3850d15412c0c04f3f6171a9d27eaf78ad7dd488
Bobby <tkdgus1797@gmail.com>
2020-10-09 17:50:15 +0900

    initial commit - backend

---
+++ backend/src/main/kotlin/com/newhope/demo/NewHopeApplication.kt
@@ -0,0 +1,11 @@
+package com.newhope.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewHopeApplication
+
+fun main(args: Array<String>) {
+    runApplication<NewHopeApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/newhope/demo/NewHopeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.newhope.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewHopeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
