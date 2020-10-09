47c7941fb6b51bf9a087c727019a9569804b3cb0
Bobby <tkdgus1797@gmail.com>
2020-10-09 19:54:01 +0900

    swagger config

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
     implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
