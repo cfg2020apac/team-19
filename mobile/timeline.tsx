d96c0b4c2fee5b86311323ce788a4179a9885822
sunny <sunny@sunnys-MacBook-Air.local>
2020-10-09 23:18:01 +0800

    timeline and user profile page

---
+++ mobile/timeline.tsx
@@ -0,0 +1,135 @@
+import { preventAutoHide } from 'expo-splash-screen';
+import * as React from 'react';
+import { StyleSheet, Image, ScrollView, Button, SafeAreaView, TouchableOpacity } from 'react-native';
+
+import { Text, View } from '../components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <ScrollView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.topcontainer}>
+          <Text style={styles.title}>{"\n"}Benedictus Alvian Sofjan{"\n"}</Text>
+          <Image
+            style={styles.emoji}
+            source={{
+              uri: 'https://i.pinimg.com/236x/29/3b/84/293b84f3561f0f895b54554ff195ea1a.jpg',
+            }}
+          />
+          <Text></Text>
+        </View>
+        <View>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <Text style={{fontSize: 18, fontWeight: 'bold'}}>        Legal Aid</Text>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Status: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>In Progress</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Date Modified: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>dd/mm/yy</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Result: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>Court date set</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Notes: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>Divorce hearing</Text>
+          </View>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <Text style={{fontSize: 18, fontWeight: 'bold'}}>        Job coaching</Text>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Status: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>Completed</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Date Modified: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>dd/mm/yy</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Result: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>Placed in position</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Notes: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600', paddingRight: 100}}>Employer Name - ABC; Position - XYZ; Working Hours - xx hrs/week</Text>
+          </View>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <Text style={{fontSize: 18, fontWeight: 'bold'}}>        Operations</Text>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Status: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>Completed</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Date Modified: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>dd/mm/yy</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Result: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>No incident reported</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={{fontSize: 18, fontWeight: '300'}}>        Notes: </Text>
+            <Text style={{fontSize: 18, fontWeight: '600'}}>xxxxx</Text>
+          </View>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  topcontainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#b5d35d',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#9f3aa0',
+  },
+  separator: {
+    marginVertical: 10,
+    height: 2,
+    width: '100%',
+  },
+  content: {
+    fontSize: 20 ,
+    alignItems: 'flex-start',
+  },
+  emoji: {
+    width: 80,
+    height: 80,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  button:{
+    marginRight:75,
+    marginLeft:75,
+    marginTop:10,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#b5d35d',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+  buttonText:{
+      color: 'black',
+      textAlign:'center',
+      paddingLeft : 10,
+      paddingRight : 10,
+      fontSize : 18,
+  }
+
+});
--- mobile/userprofile.tsx
+++ mobile/userprofile.tsx
@@ -1,13 +1,14 @@
+import { preventAutoHide } from 'expo-splash-screen';
 import * as React from 'react';
-import { StyleSheet, Image, ScrollView } from 'react-native';
+import { StyleSheet, Image, ScrollView, Button, SafeAreaView, TouchableOpacity } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View>
-      <ScrollView>
-      <View style={styles.container}>
+    <ScrollView>
+      <SafeAreaView style={styles.container}>
+      <View style={styles.topcontainer}>
         <Text style={styles.title}>{"\n"}Benedictus Alvian Sofjan{"\n"}</Text>
         <Image
           style={styles.emoji}
@@ -15,60 +16,114 @@ export default function TabOneScreen() {
             uri: 'https://i.pinimg.com/236x/29/3b/84/293b84f3561f0f895b54554ff195ea1a.jpg',
           }}
         />
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <Text></Text>
       </View>
-      <View style={{alignItems: 'flex-start'}}>
-          <Text style={{fontSize: 18}}>{`
-            Phone Number: 6434 3299
-            Email: ben@gmail.com
-            Status: Divorce
-
-            Pending Actions: XXX
-            `}</Text>
-            
-          <Text style={{color: 'deepskyblue', fontSize: 18}}>{`
-            See timeline
-          `}</Text>
-
-          <Text style={{fontSize: 18}}>{`
-            Note: XXXXXX
-
-
-            Income:
-            Housing Status:
-            Smoking Habit:
-            Drinking Habit:
-            Gender:
-            Mental Stability:
-            `}</Text>
+      <View>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <Text style={{fontSize: 18}}>{`       Phone Number: 6434 3299
+       Email: ben@gmail.com
+       Status: Divorce`}</Text>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <View style={styles.row}>
+            <Text style={{fontSize: 18}}>       Pending Actions: </Text>
+            <Button
+              onPress={() => {}}
+              title="See Timeline"
+              color="deepskyblue"
+            />
+          </View>
+          <Text style={{fontSize: 18}}>       Notes: xxxxxxxx</Text>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <Text style={{fontSize: 18}}>       Document List</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {}}>
+            <Text style={styles.buttonText}>PASSPORT</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {}}>
+            <Text style={styles.buttonText}>NRIC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {}}>
+            <Text style={styles.buttonText}>INCOME DOCUMENTS</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {}}>
+            <Text style={styles.buttonText}>OTHERS</Text>
+          </TouchableOpacity>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {}}>
+            <Text style={{
+              color: '#9f3aa0',
+              textAlign:'center',
+              paddingLeft : 10,
+              paddingRight : 10,
+              fontSize : 18
+            }}>EDIT CLIENT</Text>
+          </TouchableOpacity>
+          <Text></Text>
         </View>
-      </ScrollView>
-    </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+  },
+  topcontainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    
+    backgroundColor: '#b5d35d',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#9f3aa0',
   },
   separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    marginVertical: 10,
+    height: 2,
+    width: '100%',
   },
   content: {
     fontSize: 20 ,
     alignItems: 'flex-start',
   },
   emoji: {
-    width: 70,
-    height: 70,
+    width: 80,
+    height: 80,
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  button:{
+    marginRight:75,
+    marginLeft:75,
+    marginTop:10,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#b5d35d',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+  buttonText:{
+      color: 'black',
+      textAlign:'center',
+      paddingLeft : 10,
+      paddingRight : 10,
+      fontSize : 18,
+  }
+
 });
